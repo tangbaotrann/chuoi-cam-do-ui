7098fa39ef812bcb2fbc6afbf713988bb506b617
tangbaotrann <tangbaotrann@gmail.com>
2023-01-12 23:45:18 +0700

    updated: test git

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-center text-5xl my-4">Chuỗi cầm đồ</h1>;
+  return <h1 className="text-center text-5xl my-6">Chuỗi cầm đồ ...</h1>;
 }
 
 export default App;
